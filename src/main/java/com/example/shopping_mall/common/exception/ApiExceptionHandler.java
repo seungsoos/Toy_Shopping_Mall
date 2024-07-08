76cbeb9c9ee9ad536a00d80f70f05a6c4009bfd6
seungsoo <seung_soos@naver.com>
2024-07-08 10:38:54 +0900

    feat : ExceptionHandler 수정

--- src/main/java/com/example/shopping_mall/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/shopping_mall/common/exception/ApiExceptionHandler.java
@@ -3,26 +3,27 @@ package com.example.shopping_mall.common.exception;
 import com.example.shopping_mall.common.ResultCodeType;
 import com.example.shopping_mall.common.response.Common;
 import com.example.shopping_mall.common.response.CommonResponseDto;
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = RootException.class)
-    public CommonResponseDto rootException(Exception ex) {
-        return new CommonResponseDto(new Common(ResultCodeType.SERVER_ERROR_4S000000), ex.getMessage());
+    public ResponseEntity<CommonResponseDto<Object>> rootException(HttpServletRequest request, RootException rootException) {
+        log.error("> [Request] : {}", request.getRequestURI());
+        Common common = new Common(rootException.getResultCodeType());
+        return ResponseEntity.ok(new CommonResponseDto<>(common));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = Exception.class)
-    public CommonResponseDto exception(Exception ex) {
-        return new CommonResponseDto(new Common(ResultCodeType.SERVER_ERROR_5S000000), ex.getMessage());
+    public ResponseEntity<CommonResponseDto<Object>> exception(HttpServletRequest request) {
+        log.error("> [Request] : {}", request.getRequestURI());
+        Common common = new Common(ResultCodeType.SERVER_ERROR_5S000000);
+        return ResponseEntity.ok(new CommonResponseDto<>(common));
     }
 }
